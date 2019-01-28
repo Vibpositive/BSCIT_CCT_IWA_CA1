5e403b28c10e8e2c5118d9aadcdb6f6aa0781234
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-01-28 01:10:24 +0000

    Simple refactor of main app files

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,3 @@
-/**
-* src/db.ts
-* MIT
-*
-* long description for the file
-*
-* @summary short description for the file
-* @author Gabriel Oliveira
-*
-* Created at     : 2019-01-21 15:26:38 
- * Last modified  : 2019-01-21 16:08:45
-*/
-
 import { createConnection } from "typeorm";
 import { Comment } from "./backend/entities/comment";
 import { User } from "./backend/entities/user";
